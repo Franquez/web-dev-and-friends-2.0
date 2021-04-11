7e012506f79639eaaac648cebc84f2ed8943a079
Franquez <Franquez2000@gmail.com>
2021-04-11 19:27:54 -0300

    Initialize repository

---
+++ index.js
@@ -0,0 +1,12 @@
+alert ("Saludos, Bienvenido a La Pizza y el Pancho!");
+// En esta variable se guarda lo que el cliente escoge.
+let order = prompt("¿Qué deseas comer: pizza o pancho?");
+// En estas dos variables se guarda el precio de los productos.
+let panchoPrice = 50;
+let pizzaPrice = 75;
+if (order==="pancho") {
+    alert ("Usted eligió " + order + " y su precio es de: $" + panchoPrice);
+}
+if (order==="pizza") {
+    alert ("Usted eligió " + order + " y su precio es de: $" + pizzaPrice);
+}
