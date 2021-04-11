e4c3e351f9da002f8420d05bf5b356463886fe3f
Franquez <Franquez2000@gmail.com>
2021-04-11 20:19:41 -0300

    Add a comment for example purpose

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ alert ("Saludos, Bienvenido a La Pizza y el Pancho!");
 // En esta variable se guarda lo que el cliente escoge.
 let order = prompt("¿Qué deseas comer: pizza o pancho?");
 // En estas dos variables se guarda el precio de los productos.
+
+// Un comentario de prueba
 let panchoPrice = 50;
 let pizzaPrice = 75;
 if (order==="pancho") {
